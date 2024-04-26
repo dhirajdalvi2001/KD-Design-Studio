5bc42b3e113c497e3700344ddb2e97088f20b60c
dhirajdalvi2001 <dhirajdalvi2001@gmail.com>
2024-04-26 22:07:56 +0530

    Updated images for both products

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,127 +1,127 @@
 export const socials = {
-  facebook: "https://www.facebook.com/profile.php?id=61557391481554",
-  instagram: "https://www.instagram.com/_kd_studio__/",
+  facebook: 'https://www.facebook.com/profile.php?id=61557391481554',
+  instagram: 'https://www.instagram.com/_kd_studio__/',
 };
 
 export const aboutImg =
-  "https://res.cloudinary.com/dzwubvbs6/image/upload/v1711876001/KD%20Design%20Studio/About/about-image.jpg";
+  'https://res.cloudinary.com/dzwubvbs6/image/upload/v1711876001/KD%20Design%20Studio/About/about-image.jpg';
 
 export const contactImg =
-  "https://res.cloudinary.com/dzwubvbs6/image/upload/v1711876986/KD%20Design%20Studio/Contact/contact-image.png";
+  'https://res.cloudinary.com/dzwubvbs6/image/upload/v1711876986/KD%20Design%20Studio/Contact/contact-image.png';
 
 export const menuItems = [
-  { title: "Home", href: "/" },
-  { title: "Products", href: "/products" },
-  { title: "About", href: "/about" },
-  { title: "Contact", href: "/contact" },
+  { title: 'Home', href: '/' },
+  { title: 'Products', href: '/products' },
+  { title: 'About', href: '/about' },
+  { title: 'Contact', href: '/contact' },
 ];
 
 export const carousalProducts = [
   {
     id: 1,
-    name: "Simpliseat",
-    slug: "simpliseat",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510883/KD%20Design%20Studio/Simpliseat/izqyvv7js4lv8jl95qgj.jpg",
+    name: 'Simpliseat',
+    slug: 'simpliseat',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147474/KD%20Design%20Studio/Simpliseat/simpliseat-image-1.webp',
   },
   {
     id: 2,
-    name: "Bedside Memoir Side Table",
-    slug: "begside-momoir-side-table",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508159/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/wpz4powqrgl7j2d0xa1d.jpg",
+    name: 'Bedside Memoir Side Table',
+    slug: 'begside-momoir-side-table',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149235/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-1.webp',
   },
   {
     id: 3,
-    name: "Simpliseat",
-    slug: "simpliseat",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510883/KD%20Design%20Studio/Simpliseat/xvsbph26q3eburgietmi.jpg",
+    name: 'Simpliseat',
+    slug: 'simpliseat',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147473/KD%20Design%20Studio/Simpliseat/simpliseat-image-2.webp',
   },
   {
     id: 4,
-    name: "Bedside Memoir Side Table",
-    slug: "begside-momoir-side-table",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508162/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/r7slgidhnx6ctbl0rxiq.jpg",
+    name: 'Bedside Memoir Side Table',
+    slug: 'begside-momoir-side-table',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149275/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-2.webp',
   },
   {
     id: 5,
-    name: "Simpliseat",
-    slug: "simpliseat",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510253/KD%20Design%20Studio/Simpliseat/rwr7syu4sj7oiasapzn9.jpg",
+    name: 'Simpliseat',
+    slug: 'simpliseat',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147474/KD%20Design%20Studio/Simpliseat/simpliseat-image-3.webp',
   },
   {
     id: 6,
-    name: "Bedside Memoir Side Table",
-    slug: "begside-momoir-side-table",
-    src: "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508164/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/jevjiuhyucoowpou6v26.jpg",
+    name: 'Bedside Memoir Side Table',
+    slug: 'begside-momoir-side-table',
+    src: 'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149277/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-3.webp',
   },
 ];
 
 export const products = [
   {
     id: 1,
-    slug: "simpliseat",
-    name: "Simpliseat",
+    slug: 'simpliseat',
+    name: 'Simpliseat',
     year: 2024,
     thumbnail:
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1711809326/KD%20Design%20Studio/Simpliseat/thumbnail.svg",
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1711809326/KD%20Design%20Studio/Simpliseat/thumbnail.svg',
     imgUrls: [
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510883/KD%20Design%20Studio/Simpliseat/izqyvv7js4lv8jl95qgj.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510883/KD%20Design%20Studio/Simpliseat/xvsbph26q3eburgietmi.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510253/KD%20Design%20Studio/Simpliseat/rwr7syu4sj7oiasapzn9.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510882/KD%20Design%20Studio/Simpliseat/xyor4hizalvm7um3rie7.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712510255/KD%20Design%20Studio/Simpliseat/fh80dafuhb0izmwaprwr.jpg",
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147474/KD%20Design%20Studio/Simpliseat/simpliseat-image-1.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147473/KD%20Design%20Studio/Simpliseat/simpliseat-image-2.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147474/KD%20Design%20Studio/Simpliseat/simpliseat-image-3.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147473/KD%20Design%20Studio/Simpliseat/simpliseat-image-4.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714147474/KD%20Design%20Studio/Simpliseat/simpliseat-image-5.webp',
     ],
     details: [
       {
         id: 1,
-        title: "Introduction",
+        title: 'Introduction',
         caption: [
-          "The Simplyseat chair design aims to be simple yet functional approach to seating solutions. The concept prioritizes minimal joints and ease of assembly, allowing for efficient manufacturing and versatility in material choices.",
+          'The Simplyseat chair design aims to be simple yet functional approach to seating solutions. The concept prioritizes minimal joints and ease of assembly, allowing for efficient manufacturing and versatility in material choices.',
         ],
       },
       {
         id: 2,
-        title: "Concept Overview",
+        title: 'Concept Overview',
         caption: [
-          "The core idea behind the Simplyseat chair is to reduce integrity and aesthetic appeal. By minimizing joints, the assembly accessible to both manufacturers and consumers alike. Additionally, the modular nature of the design allows for customization and designers to create unique variations of the chair to suit various preferences and environments.",
+          'The core idea behind the Simplyseat chair is to reduce integrity and aesthetic appeal. By minimizing joints, the assembly accessible to both manufacturers and consumers alike. Additionally, the modular nature of the design allows for customization and designers to create unique variations of the chair to suit various preferences and environments.',
         ],
       },
       {
         id: 3,
-        title: "Key Features",
+        title: 'Key Features',
         listItems: [
           {
             id: 31,
-            title: "Minimal Joints",
+            title: 'Minimal Joints',
             caption: [
-              "The chair design emphasizes simplicity in construction, utilizing as few joints as possible without compromising stability. This approach streamlines the assembly process and reduces the likelihood of structural weaknesses over time.",
+              'The chair design emphasizes simplicity in construction, utilizing as few joints as possible without compromising stability. This approach streamlines the assembly process and reduces the likelihood of structural weaknesses over time.',
             ],
           },
           {
             id: 32,
-            title: "Modular Design",
+            title: 'Modular Design',
             caption: [
-              "The chair is designed to be modular, enabling easy customization and adaptation to different settings. Each component can be easily interchanged, allowing for a variety of configurations and material combinations.",
+              'The chair is designed to be modular, enabling easy customization and adaptation to different settings. Each component can be easily interchanged, allowing for a variety of configurations and material combinations.',
             ],
           },
           {
             id: 33,
-            title: "Easy Assembly",
+            title: 'Easy Assembly',
             caption: [
-              "With minimal joints and a straightforward assembly process, the chair can be easily built by consumers without the need for specialized tools or expertise. Clear instructions and intuitive design ensure a hassle-free assembly experience.",
+              'With minimal joints and a straightforward assembly process, the chair can be easily built by consumers without the need for specialized tools or expertise. Clear instructions and intuitive design ensure a hassle-free assembly experience.',
             ],
           },
           {
             id: 34,
-            title: "Material Versatility",
+            title: 'Material Versatility',
             caption: [
-              "The modular nature of the design allows for the use of various materials, including wood, metal, plastic, and composite materials. This versatility enables designers to explore different textures, colors, and finishes to create unique chair variants.",
+              'The modular nature of the design allows for the use of various materials, including wood, metal, plastic, and composite materials. This versatility enables designers to explore different textures, colors, and finishes to create unique chair variants.',
             ],
           },
           {
             id: 35,
-            title: "Sustainable Design",
+            title: 'Sustainable Design',
             caption: [
-              "The minimalist approach to design reduces material waste and energy consumption during manufacturing. Additionally, the modular design promotes longevity and repairability, contributing to a more sustainable product lifecycle.",
+              'The minimalist approach to design reduces material waste and energy consumption during manufacturing. Additionally, the modular design promotes longevity and repairability, contributing to a more sustainable product lifecycle.',
             ],
           },
         ],
@@ -130,20 +130,20 @@ export const products = [
   },
   {
     id: 2,
-    slug: "begside-momoir-side-table",
-    name: "Bedside Memoir Side Table",
+    slug: 'begside-momoir-side-table',
+    name: 'Bedside Memoir Side Table',
     year: 2024,
     thumbnail:
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712426405/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/thumbnail.svg",
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1712426405/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/thumbnail.svg',
     imgUrls: [
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508159/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/wpz4powqrgl7j2d0xa1d.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508162/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/r7slgidhnx6ctbl0rxiq.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508164/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/jevjiuhyucoowpou6v26.jpg",
-      "https://res.cloudinary.com/dzwubvbs6/image/upload/v1712508162/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/gcemn8ziljmn9quwdqnf.jpg",
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149235/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-1.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149275/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-2.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149277/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-3.webp',
+      'https://res.cloudinary.com/dzwubvbs6/image/upload/v1714149282/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/chair-image-4.webp',
     ],
     videoUrls: [
-      "https://res.cloudinary.com/dzwubvbs6/video/upload/v1712508172/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/gqex6c24d6t1hytfcjno.mp4",
-      "https://res.cloudinary.com/dzwubvbs6/video/upload/v1712508168/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/b4ejdmhua1zmx8bvwsnf.mp4",
+      'https://res.cloudinary.com/dzwubvbs6/video/upload/v1712508172/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/gqex6c24d6t1hytfcjno.mp4',
+      'https://res.cloudinary.com/dzwubvbs6/video/upload/v1712508168/KD%20Design%20Studio/Bedside%20Memoir%20Side%20Table/b4ejdmhua1zmx8bvwsnf.mp4',
     ],
     details: [
       {
