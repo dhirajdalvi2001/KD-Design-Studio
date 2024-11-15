18f1b2392c58471a3f0c856c4c40f5a6a7d6dc76
dhirajdalvipixelotech <dhiraj.dalvi@pixelotech.com>
2024-11-16 00:30:52 +0530

    update_production.sh added

--- update_production.sh
+++ update_production.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Navigate to project directory if not already there
-if [ "$(pwd)" != "$HOME/kd-studio-fe" ]; then
-  cd ~/kd-studio-fe
+if [ "$(pwd)" != "/home/ubuntu/kd-studio-fe" ]; then
+  cd /home/ubuntu/kd-studio-fe
 fi
 
 # Pull latest changes from production branch
@@ -10,11 +10,11 @@ git checkout production
 git pull origin production
 
 # Stop and remove existing container
-docker stop react-frontend-container
-docker rm react-frontend-container
+sudo docker stop react-frontend-container
+sudo docker rm react-frontend-container
 
 # Rebuild and run Docker container
-docker build -t react-frontend .
-docker run -d -p 80:80 --name react-frontend-container react-frontend
+sudo docker build -t react-frontend .
+sudo docker run -d -p 80:80 --name react-frontend-container react-frontend
 
 echo "Production updated successfully!"
