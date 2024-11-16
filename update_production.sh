3a87fe8738c462f319efa121d6c745d73f38a2a2
dhirajdalvipixelotech <dhiraj.dalvi@pixelotech.com>
2024-11-16 15:50:32 +0530

    nginx update

--- update_production.sh
+++ update_production.sh
@@ -2,32 +2,19 @@
 
 # Navigate to project directory if not already there
 if [ "$(pwd)" != "/home/ubuntu/kd-studio-fe" ]; then
-  cd /home/ubuntu/kd-studio-fe || { echo "Failed to navigate to /home/ubuntu/kd-studio-fe"; exit 1; }
+  cd /home/ubuntu/kd-studio-fe
 fi
 
 # Pull latest changes from production branch
 git checkout production
-git pull origin production || { echo "Git pull failed"; exit 1; }
+git pull origin production
 
-# Check if the container exists and stop it
-existing_container=$(sudo docker ps -aq -f name=react-frontend-container)
+# Stop and remove existing container
+sudo docker stop react-frontend-container
+sudo docker rm react-frontend-container
 
-if [ -n "$existing_container" ]; then
-  echo "Stopping existing container..."
-  sudo docker stop react-frontend-container || { echo "Failed to stop existing container"; exit 1; }
-  
-  echo "Removing existing container..."
-  sudo docker rm react-frontend-container || { echo "Failed to remove existing container"; exit 1; }
-else
-  echo "No existing container found."
-fi
-
-# Rebuild Docker image
-echo "Building new Docker image..."
-sudo docker build -t react-frontend . || { echo "Docker build failed"; exit 1; }
-
-# Run the new container
-echo "Running the new Docker container..."
-sudo docker run -d -p 80:80 --name react-frontend-container react-frontend || { echo "Failed to start new container"; exit 1; }
+# Rebuild and run Docker container
+sudo docker build -t react-frontend .
+sudo docker run -d -p 80:80 --name react-frontend-container react-frontend
 
 echo "Production updated successfully!"
