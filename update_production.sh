6c34ca6e46f4952df8724c5e61568ca7e08361e2
dhirajdalvipixelotech <dhiraj.dalvi@pixelotech.com>
2024-11-17 00:51:17 +0530

    Dockerfile updated

--- update_production.sh
+++ update_production.sh
@@ -13,6 +13,15 @@ sudo docker rm react-frontend-container
 sudo docker build -t react-frontend . || { echo "Error during build"; exit 1; }
 
 # Run Docker container
-sudo docker run -d -p 80:80 --name react-frontend-container react-frontend || { echo "Error during run"; sudo docker logs react-frontend-container; exit 1; }
+# The -v flag mounts the host directory /etc/letsencrypt/live/kd-studio.in into the container at the same path.
+# The :ro option ensures the files are mounted as read-only.
+sudo docker run -d \
+  --name react-frontend-container \
+  -p 80:80 \
+  -p 443:443 \
+  -v /etc/letsencrypt/live/kd-studio.in:/etc/letsencrypt/live/kd-studio.in:ro \
+  react-frontend
+ || { echo "Error during run"; sudo docker logs react-frontend-container; exit 1; }
+
 
 echo "Production updated successfully!"
