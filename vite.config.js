6b4c00201c0227f42b48cb361ef03007431760c6
dhirajdalvi2001 <dhirajdalvi2001@gmail.com>
2025-03-31 01:18:27 +0530

    Minor updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
